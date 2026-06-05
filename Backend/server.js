a1b9464e7f46ed44812fc21043776a45262955cd
josedaniel <josdanielcch@gmail.com>
2026-06-05 16:28:21 -0400

    fix: explicit host binding to 0.0.0.0

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,6 @@
 const app = require('./src/app'); // Importa la app configurada
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Servidor corriendo en puerto ${PORT}`);
 });
